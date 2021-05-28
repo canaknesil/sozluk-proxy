78e5f2b9f60b4c2942039be0b5bce6bb7d742017
Can Aknesil <aknesil@kth.se>
2021-05-29 00:49:06 +0200

    CORS headers application

--- sozluk_proxy_site/sozluk_proxy_site/settings.py
+++ sozluk_proxy_site/sozluk_proxy_site/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # Must be as high as possible, before any middleware that can generate responses.
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# corsheaders
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://sozluk.canaknesil.com",
+# ]
+
